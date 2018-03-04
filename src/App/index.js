2d2292413851baad5c553e437925c154d0631202
Christian Deinert <ChDeinert@gmail.com>
2018-03-04 21:04:32 +0100

    Remove currently? unnessesary Layout component

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import Layout from 'Components/Layout';
+import React, { Fragment } from 'react';
+import Header from 'Components/Header';
+import Footer from 'Components/Footer';
+
 import './base.css';
 
 const App = () => (
-  <Layout>
+  <Fragment>
+    <Header />
     <h1>Hello World!</h1>
-  </Layout>
+    <Footer />
+  </Fragment>
 );
 
 export default App;
