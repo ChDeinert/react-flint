edf8f6edf4fc2d7cb0c41cf6b7626007560998fc
Christian Deinert <ChDeinert@gmail.com>
2018-03-04 11:31:10 +0100

    Move common webpack config to config-base

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,77 +1,68 @@
-const path = require('path');
 const webpack = require('webpack');
 const cssImport = require('postcss-import');
 const cssnext = require('postcss-cssnext');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const publicPath = '/assets';
+const webpackConfig = require('./webpack.config.base');
 
-module.exports = {
-  target: 'web',
-  entry: [
-    require.resolve('./polyfills'),
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client',
-    './entrypoint/client/dev.js',
-  ],
-  devtool: 'source-map',
-  output: {
-    filename: 'js/[name].js',
-    path: path.resolve(__dirname, '../build'),
-    publicPath,
-    pathinfo: true,
+webpackConfig.entry.push(
+  'react-hot-loader/patch',
+  'webpack-hot-middleware/client',
+  './entrypoint/client/dev.js',
+);
+webpackConfig.devtool = 'source-map';
+webpackConfig.output.filename = 'js/[name].js';
+webpackConfig.output.pathinfo = true;
+webpackConfig.module.rules = [
+  {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['env', 'react'],
+        cacheDirectory: true,
+        plugins: ['react-hot-loader/babel'],
+      },
+    },
   },
-  module: {
-    rules: [
+  {
+    test: /\.css$/,
+    use: [
+      require.resolve('style-loader'),
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'react'],
-            cacheDirectory: true,
-            plugins: ['react-hot-loader/babel'],
-          },
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 1,
+          modules: true,
+          localIdentName: '[name]__[local]___[hash:base64:5]',
         },
       },
       {
-        test: /\.css$/,
-        use: [
-          require.resolve('style-loader'),
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              importLoaders: 1,
-              modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-            },
-          },
-          {
-            loader: require.resolve('postcss-loader'),
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                cssImport(),
-                cssnext({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'not ie < 9',
-                  ],
-                }),
+        loader: require.resolve('postcss-loader'),
+        options: {
+          ident: 'postcss',
+          plugins: () => [
+            cssImport(),
+            cssnext({
+              browsers: [
+                '>1%',
+                'last 4 versions',
+                'Firefox ESR',
+                'not ie < 9',
               ],
-            },
-          },
-        ],
+            }),
+          ],
+        },
       },
     ],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
-  ],
-};
+];
+webpackConfig.plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new BundleAnalyzerPlugin({
+    openAnalyzer: false,
+  }),
+];
+
+module.exports = webpackConfig;
--- config/webpack.config.prod.ssr.server.js
+++ config/webpack.config.prod.ssr.server.js
@@ -1,114 +1,45 @@
-const path = require('path');
 const webpack = require('webpack');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const cssImport = require('postcss-import');
-const cssnext = require('postcss-cssnext');
 
-const publicPath = '/assets';
+const webpackConfig = require('./webpack.config.base');
 
 const cssFilename = '[name].css';
 
-module.exports = {
-  target: 'node',
-  entry: {
-    app: ['./src/App'],
-  },
-  devtool: 'nosources-source-map',
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../build'),
-    publicPath,
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        oneOf: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['env', 'react'],
-                cacheDirectory: true,
-              },
-            },
-          },
-          {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract({
-              fallback: {
-                loader: require.resolve('style-loader'),
-                options: {
-                  hmr: false,
-                },
-              },
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                    importLoaders: 1,
-                    minimize: true,
-                    sourceMap: true,
-                    modules: true,
-                    localIdentName: '[name]__[local]___[hash:base64:5]',
-                  },
-                },
-                {
-                  loader: require.resolve('postcss-loader'),
-                  options: {
-                    ident: 'postcss',
-                    plugins: () => [
-                      cssImport(),
-                      cssnext({
-                        browsers: [
-                          '>1%',
-                          'last 4 versions',
-                          'Firefox ESR',
-                          'not ie < 9', // React doesn't support IE8 anyway
-                        ],
-                      }),
-                    ],
-                  },
-                },
-              ],
-            }),
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        comparisons: false,
-      },
-      mangle: {
-        safari10: true,
-      },
-      output: {
-        comments: false,
-        ascii_only: true,
-      },
-      sourceMap: true,
-    }),
-    new ExtractTextPlugin({
-      filename: cssFilename,
-    }),
-    new ManifestPlugin({
-      fileName: 'asset-manifest.ssr.json',
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-      reportFilename: 'report.ssr.html',
-    }),
-  ],
-};
+webpackConfig.target = 'node';
+webpackConfig.entry = { app: ['./src/App'] };
+webpackConfig.output.filename = '[name].js';
+webpackConfig.output.libraryTarget = 'commonjs2';
+webpackConfig.plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': '"production"',
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      comparisons: false,
+    },
+    mangle: {
+      safari10: true,
+    },
+    output: {
+      comments: false,
+      ascii_only: true,
+    },
+    sourceMap: true,
+  }),
+  new ExtractTextPlugin({
+    filename: cssFilename,
+  }),
+  new ManifestPlugin({
+    fileName: 'asset-manifest.ssr.json',
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: 'report.ssr.html',
+  }),
+];
+
+module.exports = webpackConfig;
