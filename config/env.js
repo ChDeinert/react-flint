b7284f3fac196b6248865963978544d547f4c4f9
Christian Deinert <ChDeinert@gmail.com>
2018-09-24 23:42:36 +0200

    Fix linter

--- config/env.js
+++ config/env.js
@@ -1,9 +1,4 @@
-const fs = require('fs');
-
 const dotEnvPath = process.env.DOT_ENV_PATH || '.env';
-fs.exists(dotEnvPath, exists =>
-  // eslint-disable-next-line no-console
-  console.log(exists ? 'Info: Using %s' : 'Info: %s not found, using defaults', dotEnvPath));
 require('dotenv').load({ path: dotEnvPath, silent: true });
 
 module.exports = {
--- src/server/renderer.ssr.js
+++ src/server/renderer.ssr.js
@@ -1,7 +1,6 @@
 const ReactDOMServer = require('react-dom/server');
 
-module.exports = ({ template, App }) => () =>
-  template.replace(
-    '<div id="main"></div>',
-    `<div id="main">${ReactDOMServer.renderToString(App())}</div>`,
-  );
+module.exports = ({ template, App }) => () => template.replace(
+  '<div id="main"></div>',
+  `<div id="main">${ReactDOMServer.renderToString(App())}</div>`,
+);
